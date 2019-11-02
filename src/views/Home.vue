c93efec480848877acd4e3665cc476c7583d6d20
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-11-03 03:11:40 +0530

    Homepage Form Added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,39 @@
   <section class="hero is-fullheight-with-navbar">
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">Hero title</h1>
-        <h2 class="title">Hero Subtitle</h2>
+        <div class="row">
+          <div class="column">
+            <h1 class="title">Project Spam</h1>
+            <h2 class="subtitle">Spam Everybody</h2>
+          </div>
+          <div class="column">
+            <div class="field has-addons">
+              <div class="control is-expanded is-loading has-icons-left">
+                <input type="text" class="input" placeholder="Link to Blog..." />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-link" />
+                </span>
+              </div>
+              <div class="control">
+                <a class="button is-primary">Search</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="column">
+            <div class="field is-grouped">
+              <div class="control is-expanded is-loading has-icons-left">
+                <input type="text" class="input" placeholder="Link to Blog..." />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-link" />
+                </span>
+              </div>
+              <div class="control">
+                <a class="button is-primary">Search</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
